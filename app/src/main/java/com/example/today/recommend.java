2ac3406c42602e8b03b6b0984a726161e7d9c158
Jyeontae <jyt5768@hanmail.net>
2019-04-13 00:42:40 +0900

    로그인버튼 눌렀을 시 로딩창으로 가는것까지 구현

---
+++ app/src/main/java/com/example/today/recommend.java
@@ -0,0 +1,34 @@
+package com.example.today;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class recommend extends AppCompatActivity {
+    /*private ViewGroup mapLayout;
+    private NMapView mMapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init(){
+
+
+        mapLayout = findViewById(R.id.mapLayout);
+
+        mMapView = new NMapView(this);
+        mMapView.setClientId(getResources().getString(R.string.NAVER_API_KEY)); // 클라이언트 아이디 값 설정
+        mMapView.setClickable(true);
+        mMapView.setEnabled(true);
+        mMapView.setFocusable(true);
+        mMapView.setFocusableInTouchMode(true);
+        mMapView.setScalingFactor(1.5f);
+        mMapView.requestFocus();
+
+        mapLayout.addView(mMapView);
+    } */
+}
